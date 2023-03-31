5ff9d5a9cb35687e1a5e1a546da9ccf95a4c453b
Jay Girardot <jasongirardot.adm@gmail.com>
2023-03-31 14:04:59 +0200

    docs: responsive laptop

--- js/main.js
+++ js/main.js
@@ -57,3 +57,36 @@ center: [47.218371, -1.553621], // Coordonnées de Nantes, France
 layers: L.mapquest.tileLayer('map'),
 zoom: 12
 });
+
+
+
+
+ // Sélectionnez tous les éléments de navigation avec un identifiant commençant par "nav-"
+ const navItems = document.querySelectorAll('[id^="nav-"]');
+
+ // Stockez les couleurs d'origine des icônes SVG
+ const originalColors = {
+   'nav-about': 'rgba(255, 255, 255, 0.74)',
+   'nav-resume': 'rgba(255, 255, 255, 0.74)',
+   'nav-portfolio': 'rgba(255, 255, 255, 0.74)',
+   'nav-contact': 'rgba(255, 255, 255, 0.74)',
+ };
+
+ // Ajoutez un écouteur d'événements 'click' à chaque élément de navigation
+ navItems.forEach((navItem) => {
+   navItem.addEventListener('click', (event) => {
+     // Restaurez la couleur d'origine de tous les éléments
+     navItems.forEach((otherNavItem) => {
+       const otherSvg = otherNavItem.querySelector('svg');
+       otherSvg.setAttribute('fill', originalColors[otherNavItem.id]);
+     });
+
+     // Changez la couleur de remplissage de l'icône SVG dans l'élément cliqué
+     const svg = event.target.closest('a').querySelector('svg');
+     svg.setAttribute('fill', '#12FF0D');
+   });
+ });
+
+
+
+
